d4b2852293e0bb7137eb143c331837edca6388eb
Lars Aksel Opsahl <lop@skogoglandskap.no>
2019-12-03 14:52:38 +0100

    Added code for using content based grid

--- src/test/sql/regress/find_overlap_and_gap-pre.pl
+++ src/test/sql/regress/find_overlap_and_gap-pre.pl
@@ -12,19 +12,10 @@ for my $file (glob '../../../main/sql/func*') {
 	copy_file_into($file,$fh_out);
 }
 
-# get def for conetnet based grid
-if ( -e '/Users/lop/dev/github/content_balanced_grid/func_grid' ) 
-{
-	# TODO find another way to to get data from github
-	for my $file (glob '/Users/lop/dev/github/content_balanced_grid/func_grid/func*.sql') {
-		copy_file_into($file,$fh_out);
-	}
-} 
-else
-{
-	copy_file_into('find_overlap_gap-pre-cbg-def.sql',$fh_out);
-	print "use the find_overlap_gap-pre-cbg-def.sql \n";
-}
+# get def for content based grid, TODO find another way to pick up this from https://github.com/larsop/content_balanced_grid
+copy_file_into('find_overlap_gap-pre-cbg-def.sql',$fh_out);
+print "use the find_overlap_gap-pre-cbg-def.sql \n";
+
 
 copy_file_into('find_overlap_and_gap-pre-def.sql',$fh_out);
 copy_file_into('overlap_gap_input_t1.sql',$fh_out);
