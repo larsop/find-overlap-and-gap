d1c65af2f3179cd9585fa0205e9049c21292689f
Lars Aksel Opsahl <lop@skogoglandskap.no>
2019-12-03 13:05:08 +0100

    Added more tests and some info

--- src/test/sql/regress/find_overlap_and_gap.sql
+++ src/test/sql/regress/find_overlap_and_gap.sql
@@ -4,15 +4,24 @@
 -- Test that input data are ok
 SELECT '1', count(*) from test_data.overlap_gap_input_t1;
 
--- Pipe output sql to to files sp they can executed
+-- Pipe output sql to a file to execute later 
 \o /tmp/run_cmd.sql
 SELECT find_overlap_gap_make_run_cmd('test_data.overlap_gap_input_t1','geom',4258,'test_data.overlap_gap_input_t1_res',50);
 
+-- Run with GNU parallel 
 \! parallel -j 4  psql postgis_reg  -c :::: /tmp/run_cmd.sql > /tmp/run_cmd_result.log
-
-
 \o
-SELECT '2', count(*) antall_overlap, sum(st_area(ST_Transform(geom,32633))) from (SELECT  (ST_dump(geom)).geom as geom, cell_id from test_data.overlap_gap_input_t1_res_overlap) as r where ST_Area(geom) >0;                  
 
-SELECT '3',  count(*) antall_gap, sum(st_area(ST_Transform(geom,32633))) from (SELECT  (ST_dump(geom)).geom as geom, cell_id from test_data.overlap_gap_input_t1_res_gap) as r;                  
+-- Check the result
+SELECT 'check overlap table', count(*) num_overlap, sum(st_area(ST_Transform(geom,32633))) from (SELECT  (ST_dump(geom)).geom as geom, cell_id 
+from test_data.overlap_gap_input_t1_res_overlap) as r where ST_Area(geom) >0;                  
+
+SELECT 'check gap table',  count(*) num_gap, sum(st_area(ST_Transform(geom,32633))) 
+from (SELECT  (ST_dump(geom)).geom as geom, cell_id from test_data.overlap_gap_input_t1_res_gap) as r;                  
+
+SELECT 'check grid table',  count(*) num_grid, sum(st_area(ST_Transform(geom,32633))) from (SELECT  (ST_dump(geom)).geom as geom, id 
+from test_data.overlap_gap_input_t1_res_grid) as r;                  
+
+SELECT 'check boundery table',  count(*) num_boudery, sum(st_area(ST_Transform(geom,32633))) from (SELECT  (ST_dump(geom)).geom as geom, id 
+from test_data.overlap_gap_input_t1_res_boundery) as r;                  
 
