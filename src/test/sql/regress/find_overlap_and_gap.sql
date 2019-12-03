f6dccd8c016ec17b6185313381a5362684b18d11
Lars Aksel Opsahl <lop@skogoglandskap.no>
2019-12-03 12:39:14 +0100

    Added more test

--- src/test/sql/regress/find_overlap_and_gap.sql
+++ src/test/sql/regress/find_overlap_and_gap.sql
@@ -4,9 +4,15 @@
 -- Test that input data are ok
 SELECT '1', count(*) from test_data.overlap_gap_input_t1;
 
---psql testdb -c"\! psql -t -q -o /tmp/run_cmd.sql testdb -c\"SELECT find_overlap_gap_make_run_cmd('test_data.overlap_gap_input_t1','geom',4258,'test_data.overlap_gap_input_t1_res',50);\"; parallel -j 4  psql testdb -c :::: /tmp/run_cmd.sql" 2>> /tmp/analyze.log;
+-- Pipe output sql to to files sp they can executed
+\o /tmp/run_cmd.sql
+SELECT find_overlap_gap_make_run_cmd('test_data.overlap_gap_input_t1','geom',4258,'test_data.overlap_gap_input_t1_res',50);
 
---psql testdb -c"SELECT count(*) antall_overlap, sum(st_area(ST_Transform(geom,32633))) from (SELECT  (ST_dump(geom)).geom as geom, cell_id from test_data.overlap_gap_input_t1_res_overlap) as r where ST_Area(geom) >0";                  
+\! parallel -j 4  psql postgis_reg  -c :::: /tmp/run_cmd.sql > /tmp/run_cmd_result.log
 
---psql testdb -c"SELECT count(*) antall_gap, sum(st_area(ST_Transform(geom,32633))) from (SELECT  (ST_dump(geom)).geom as geom, cell_id from test_data.overlap_gap_input_t1_res_gap) as r";                  
+
+\o
+SELECT '2', count(*) antall_overlap, sum(st_area(ST_Transform(geom,32633))) from (SELECT  (ST_dump(geom)).geom as geom, cell_id from test_data.overlap_gap_input_t1_res_overlap) as r where ST_Area(geom) >0;                  
+
+SELECT '3',  count(*) antall_gap, sum(st_area(ST_Transform(geom,32633))) from (SELECT  (ST_dump(geom)).geom as geom, cell_id from test_data.overlap_gap_input_t1_res_gap) as r;                  
 
