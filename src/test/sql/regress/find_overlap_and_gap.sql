7d542bd847c2fa509b40dada474866edfca7e5e2
Lars Aksel Opsahl <lop@skogoglandskap.no>
2019-12-03 22:24:11 +0100

    More area tests

--- src/test/sql/regress/find_overlap_and_gap.sql
+++ src/test/sql/regress/find_overlap_and_gap.sql
@@ -2,7 +2,7 @@
 -- This is test that does a obverlap and gap test on overlap_gap_input_t1.sql
 
 -- Test that input data are ok
-SELECT '1', count(*) from test_data.overlap_gap_input_t1;
+SELECT '1', count(*), ROUND(sum(st_area(ST_Transform(geom,32633)))/1000)::integer from test_data.overlap_gap_input_t1;
 
 -- Pipe output sql to a file to execute later - \o /tmp/run_cmd.sql does not work in Travis
 SELECT find_overlap_gap_make_run_cmd('test_data.overlap_gap_input_t1','geom',4258,'test_data.overlap_gap_input_t1_res',50);
@@ -41,15 +41,15 @@ SELECT find_overlap_gap_single_cell('test_data.overlap_gap_input_t1','geom',4258
 SELECT find_overlap_gap_single_cell('test_data.overlap_gap_input_t1','geom',4258,'test_data.overlap_gap_input_t1_res',28,28);
 
 -- Check the result
-SELECT 'check overlap table', count(*) num_overlap, (sum(st_area(ST_Transform(geom,32633)))/1000)::integer from (SELECT  (ST_dump(geom)).geom as geom, cell_id 
+SELECT 'check overlap table', count(*) num_overlap, ROUND(sum(st_area(ST_Transform(geom,32633)))/10000)::integer from (SELECT  (ST_dump(geom)).geom as geom, cell_id 
 from test_data.overlap_gap_input_t1_res_overlap) as r where ST_Area(geom) >0;                  
 
-SELECT 'check gap table',  count(*) num_gap, (sum(st_area(ST_Transform(geom,32633)))/1000)::integer 
+SELECT 'check gap table',  count(*) num_gap, ROUND(sum(st_area(ST_Transform(geom,32633)))/10000)::integer 
 from (SELECT  (ST_dump(geom)).geom as geom, cell_id from test_data.overlap_gap_input_t1_res_gap) as r;                  
 
-SELECT 'check grid table',  count(*) num_grid, (sum(st_area(ST_Transform(geom,32633)))/1000)::integer from (SELECT  (ST_dump(geom)).geom as geom, id 
+SELECT 'check grid table',  count(*) num_grid, ROUND(sum(st_area(ST_Transform(geom,32633)))/10000)::integer from (SELECT  (ST_dump(geom)).geom as geom, id 
 from test_data.overlap_gap_input_t1_res_grid) as r;                  
 
-SELECT 'check boundery table',  count(*) num_boudery, (sum(st_area(ST_Transform(geom,32633)))/1000)::integer from (SELECT  (ST_dump(geom)).geom as geom, id 
+SELECT 'check boundery table',  count(*) num_boudery, ROUND(sum(st_area(ST_Transform(geom,32633)))/10000)::integer from (SELECT  (ST_dump(geom)).geom as geom, id 
 from test_data.overlap_gap_input_t1_res_boundery) as r;                  
 
