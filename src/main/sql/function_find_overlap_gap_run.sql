24d50c9245e776e6452733e672d9dd3ed034c2c5
Lars Aksel Opsahl <lop@skogoglandskap.no>
2019-12-29 09:32:54 +0100

    Added some comments

--- src/main/sql/function_find_overlap_gap_run.sql
+++ src/main/sql/function_find_overlap_gap_run.sql
@@ -4,7 +4,7 @@ DROP PROCEDURE IF EXISTS find_overlap_gap_run(
 table_to_analyze_ varchar, -- The table to analyze 
 geo_collumn_name_ varchar, 	-- the name of geometry column on the table to analyze	
 srid_ int, -- the srid for the given geo column on the table analyze
-table_name_result_prefix_ varchar, -- This is the prefix used for the result tables
+table_name_result_prefix_ varchar, -- This is table name prefix including schema used for the result tables
 max_rows_in_each_cell_ int -- this is the max number rows that intersects with box before it's split into 4 new boxes, default is 5000
 );
 
@@ -21,7 +21,11 @@ CREATE OR REPLACE PROCEDURE find_overlap_gap_run(
 table_to_analyze_ varchar, -- The table to analyze 
 geo_collumn_name_ varchar, 	-- the name of geometry column on the table to analyze	
 srid_ int, -- the srid for the given geo column on the table analyze
-table_name_result_prefix_ varchar, -- This is the prefix used for the result tables
+table_name_result_prefix_ varchar, -- This is table name prefix including schema used for the result tables
+-- || '_overlap'; -- The schema.table name for the overlap/intersects found in each cell 
+-- || '_gap'; -- The schema.table name for the gaps/holes found in each cell 
+-- || '_grid'; -- The schema.table name of the grid that will be created and used to break data up in to managle pieces
+-- || '_boundery'; -- The schema.table name the outer boundery of the data found in each cell 
 max_parallel_jobs_ int, -- this is the max number of paralell jobs to run. There must be at least the same number of free connections
 max_rows_in_each_cell_ int DEFAULT 5000 -- this is the max number rows that intersects with box before it's split into 4 new boxes, default is 5000
 ) LANGUAGE plpgsql 
--- src/test/sql/regress/find_overlap_and_gap.sql
+++ src/test/sql/regress/find_overlap_and_gap.sql
@@ -7,7 +7,14 @@ CREATE EXTENSION dblink; -- needed by  execute_parallel
 SELECT '1 spheroid-true', count(*), ROUND(sum(st_area(geom,true))::numeric,0) from test_data.overlap_gap_input_t1;
 SELECT '1 transform 3035', count(*), ROUND(sum(st_area(ST_Transform(geom,3035)))::numeric,0) from test_data.overlap_gap_input_t1;
 
--- Pipe output sql to a file to execute later - \o /tmp/run_cmd.sql does not work in Travis
+-- Call this the overlap and gap function to find overlap and gap
+-- The geometry column is named 'geom' in the test dataset and uses srid 4258, 	
+-- We place the results in test_data.overlap_gap_input_t1_res*
+-- || '_overlap'; -- The schema.table name for the overlap/intersects found in each cell 
+-- || '_gap'; -- The schema.table name for the gaps/holes found in each cell 
+-- || '_grid'; -- The schema.table name of the grid that will be created and used to break data up in to managle pieces
+-- || '_boundery'; -- The schema.table name the outer boundery of the data found in each cell 
+-- It will run with 10 paralell threads and max 50 polygons in each content based cell  
 CALL find_overlap_gap_run('test_data.overlap_gap_input_t1','geom',4258,'test_data.overlap_gap_input_t1_res',10,50);
 
 -- Check the result
